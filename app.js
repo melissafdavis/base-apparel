41ff1df8ec0055e8b6a2463a659ac7d6ee5d77c7
Melissa Davis <melissafloresdavis@gmail.com>
2020-01-19 17:12:57 -0600

    error message fix, styling

--- app.js
+++ app.js
@@ -6,26 +6,24 @@ button.addEventListener('click', validateEntry);
 
 
     function validateEntry() {
+      
       const email = document.querySelector('.email-input');
 
       const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
     
       if(!email.value || !re.test(email.value)){
         errorMessage();
-      } else {
-        alert("valid email");
-        //send to php?
-      }
+      } 
     }
 
-    function errorMessage(){
+    function errorMessage(e){
       var node= document.createElement('div');
       node.className = "errorMessage";
       var errorMessage = document.createTextNode("Please enter a valid email address");
       node.appendChild(errorMessage);
       var parent = emailDiv;
       parent.appendChild(node);
-  
+      e.preventdefault();
     }
 
     
